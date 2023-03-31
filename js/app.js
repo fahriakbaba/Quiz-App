17047c2927a2345352257673862d897fed87a77e
Fahri Akbaba <fahriakbaba@gmail.com>
2023-03-31 20:51:45 +0300

    infos are displayed from UI and added function for clicking answer

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
-// to get elements
- 
+//to get all p tags
+const allPTags = document.querySelectorAll("p");
 
-let numberOfQuestions = 0;
+//to create instances of UI
+const ui = new UI();
+
+let numberOfQuestions = 2;
 
 const questionsArray = [
-    new Quiz("What's the capital of Turkey?", ["Ankara", "İzmir", "İstanbul", "Antalya"], "Antalya"),
+    new Quiz("what is the most popular language in programming", ["C#", "JavaScript", "Python", "Java", "Swift"], "JavaScript"),
+    new Quiz("What's the capital of Turkey?", ["Ankara", "İzmir", "İstanbul", "Antalya"], "Ankara"),
     new Quiz("What's the best football team in Turkey?", ["Galatasaray", "Beşiktaş", "Trabzonspor", "Göztepe"], "Beşiktaş"),
     new Quiz("What's the best programming language in computer?", ["Java", "Javascript", "C#", "Python"], "Python"),
     new Quiz("Which degree freezes water?", ["0 C", "5 C", "100 C", "24 C"], "0 C")
 ]
 
-console.log(questionsArray[numberOfQuestions]);
+ui.showInfosToUI(questionsArray[numberOfQuestions]);
+
+allPTags.forEach(item => {
+    item.addEventListener("click", () => {ui.handleClickForAnswer(item.innerHTML)})
+})
+ 
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,16 @@
 class UI {
-    
+    constructor() {
+        this.question = document.querySelector(".quiz-question");
+        this.answers = [document.querySelector("p:nth-child(1)"), document.querySelector("p:nth-child(2)"), document.querySelector("p:nth-child(3)"), document.querySelector("p:nth-child(4)")];
+    }
+
+    showInfosToUI(infos) {
+        console.log(infos);
+        this.answers.forEach((item, index) => {
+            item.innerHTML = infos.answers[index];
+        })
+        this.question.innerHTML = infos.questions
+    }
+
+    handleClickForAnswer (element) { console.log(element) }
 }
